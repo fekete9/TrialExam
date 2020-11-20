7f1fe3c0218359acdb7a325fb11dfffce3bc9ed0
Fekete Dániel <67480073+fekete9@users.noreply.github.com>
2020-11-20 11:05:01 +0100

    AverageOfEveryNth function is ready

--- hsg-algo-exam-main/src/gfa/trialexam/solution/AverageOfEveryNth.java
+++ hsg-algo-exam-main/src/gfa/trialexam/solution/AverageOfEveryNth.java
@@ -1,4 +1,4 @@
-package com.gfa.trialexam.solution;
+package com.gfa.trialexam;
 
 public class AverageOfEveryNth {
     public static void main(String[] args) {
@@ -24,5 +24,24 @@ public class AverageOfEveryNth {
 
         Amivel vissza kell térnie: `0`, mert nincs érvényes elem.
     */
+        System.out.println("átlaga: " + averageOfEveryNth(new int[]{2, 4, 10, 34, 3, 16, 3, 21}, 3));
+        System.out.println("átlaga: " + averageOfEveryNth(new int[]{2, 4, 10, 34, 3, 16, 3, 21}, 2));
+        System.out.println("átlaga: " + averageOfEveryNth(new int[]{2, 4, 10, 34, 3, 16, 3, 21}, 10));
     }
+
+    public static float averageOfEveryNth(int[] number, int n) {
+        float count = 0;
+        float sum = 0;
+        for (int i = n - 1; i < number.length; i += n) {
+            sum += number[i];
+            count++;
+        }
+        if (count == 0) {
+            return 0;
+        } else {
+            return sum / count;
+        }
+
+    }
+
 }
